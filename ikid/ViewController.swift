97e6fdb12cf675085a37c9996effa0c950992fce
Raymond Lee <rayjaelee@gmail.com>
2018-10-23 18:17:57 -0700

    Initial Commit

---
+++ ikid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ikid
+//
+//  Created by Raymond Lee on 10/23/18.
+//  Copyright © 2018 rayjaelee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
